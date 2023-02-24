a744a16cb8e3a08e5da4bd69687b43b7fa2d5ff5
praba-ramadurai <prabha.me2017@gmail.com>
2023-02-24 16:01:30 +0530

    React Router 5 updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import { Home } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  Checkout,
+  Home,
+  SingleProduct,
+  About,
+  Products,
+  Cart,
+  Error,
+} from "./pages";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import SingleProduct from "./SingleProduct";
 import Cart from "./CartPage";
 import Checkout from "./CheckoutPage";
 import Private from "./PrivateRoute";
+import Error from "./ErrorPage";
 
-export {Home,About,Products,SingleProduct,Cart,Checkout,Private};
+export {Home,About,Products,SingleProduct,Cart,Checkout,Private,Error};
 
