2989802beddd87e87171ae2f13ac0037bc8b847a
praba-ramadurai <prabha.me2017@gmail.com>
2023-02-24 12:46:09 +0530

    Created pages

---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Private=()=>{
+   return(
+       <h1>Private</h1>
+   )
+}
+
+export default Private;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProduct";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+
+export {Home,About,Products,SingleProduct,Cart,Checkout,Private};
+
