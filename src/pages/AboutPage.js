0cdae0d76af2af904d8225e3bd5e33cd5b0cf9f5
praba-ramadurai <prabha.me2017@gmail.com>
2023-03-06 12:59:10 +0530

    About page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,58 @@
- import React from "react";
+import React from "react";
+import styled from "styled-components";
+import aboutImg from "../assets/hero.jpeg";
 
- const About=()=>{
-    return(
-        <h1>About</h1>
-    )
- }
+const About = () => {
+  return (
+    <Wrapper className="page section section-center">
+      <img src={aboutImg} alt="nice page" />
+      <article>
+        <div className="title">
+          <h2>Our Story</h2>
+          <div className="underline"></div>
+        </div>
+      </article>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+        accusantium sapiente tempora sed dolore esse deserunt eaque excepturi,
+        delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta.
+        Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt
+        sequi blanditiis est exercitationem molestiae delectus saepe odio
+        eligendi modi porro eaque in libero minus unde sapiente consectetur
+        architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum
+        totam velit saepe sed quos similique amet. Ex, voluptate accusamus
+        nesciunt totam vitae esse iste.
+      </p>
+    </Wrapper>
+  );
+};
 
- export default About;
+const Wrapper = styled.section`
+display: grid;
+gap:4rem;
+img{
+    width:100%;
+    display: block;
+    height: 500px;
+    object-fit: cover;
+    border-radius: 5px;;
+}
+p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+@media (min-width:992px){
+    grid-template-columns: 1fr 1fr;;
+}
+
+`;
+
+export default About;
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <Wrapper>
+    <Wrapper className="page-100">
       <section>
         <h1>404</h1>
         <h3>Sorry, the page you tried cannot be found</h3>
